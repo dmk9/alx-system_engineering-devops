48fb9a7f4708e48167eadb714a8db07b61e7bf7a
Dominic <usiabulu.dominic@outlook.com>
2024-04-05 00:02:11 +0100

    task 2 update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
-# Using Puppet, install flask from pip3
+#!/usr/bin/pup
+# Using Puppet, install flask (2.1.0) from pip3
 
-package { 'flask':
+package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
