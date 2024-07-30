f381a8a6d94a00cd00552087ee72e45c7484e9ee
Dominic Usiabulu <usiabulu.dominic@gmail.com>
2024-07-30 02:14:02 +0100

    api*

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,37 +1,56 @@
 #!/usr/bin/python3
 '''
-A Python script to export data in the CSV format.
+A Python script that, using this REST API, for a given employee ID,
+exports all information on tasks for selected 
+employee in the CSV format.
 '''
 
-import re  # Import the regular expression module
-import requests  # Import the requests module
-import sys  # Import the sys module
+import requests
+import sys
+import csv
 
-# The API's URL
 API_URL = 'https://jsonplaceholder.typicode.com'
 
-if __name__ == '__main__':
-    # Check if command line arguments were provided
+
+def gather_data_from_api(user_id):
+    user_res = requests.get(f'{API_URL}/users/{user_id}').json()
+    todos_res = requests.get(f'{API_URL}/todos').json()
+    return user_res, todos_res
+
+
+def main():
     if len(sys.argv) > 1:
-        # Check if the argument is a valid user ID
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            # Get the user's information from the API
-            id = int(sys.argv[1])
-            user_res = requests.get('{}/users/{}'.format(API_URL, id)).json()
-            todos_res = requests.get('{}/todos'.format(API_URL)).json()
-            user_name = user_res.get('username')
+        try:
+            employee_id = int(sys.argv[1])
+            user_res, todos_res = gather_data_from_api(employee_id)
+            user_name = user_res.get('name')
 
-            # Filter todos for the given user
-            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
+            todos = [
+                todo for todo in todos_res
+                if todo.get('userId') == employee_id
+            ]
+            todos_done = [todo for todo in todos if todo.get('completed')]
 
-            # Write the data to a CSV file
-            with open('{}.csv'.format(id), 'w') as file:
+            print(
+                f"Employee {user_name} is done with tasks "
+                f"({len(todos_done)}/{len(todos)}):"
+            )
+            for todo in todos_done:
+                print(f"\t {todo.get('title')}")
+
+            # Write to CSV
+            with open(f'{employee_id}.csv', mode='w', newline='') as file:
+                writer = csv.writer(file, quoting=csv.QUOTE_ALL)
                 for todo in todos:
-                    file.write(
-                        '"{}","{}","{}","{}"\n'.format(
-                            id,
-                            user_name,
-                            todo.get('completed'),
-                            todo.get('title')
-                        )
-                    )
+                    writer.writerow(
+                        [employee_id, user_name,
+                         todo.get('completed'), todo.get('title')])
+
+        except ValueError:
+            print("Please provide a valid employee ID.")
+    else:
+        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
+
+
+if __name__ == "__main__":
+    main()
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python3
+'''
+A Python script that, using this REST API, for a given employee ID,
+export all information on tasks for selected 
+employee in the JSON format.
+'''
 import requests
 import sys
 import json
