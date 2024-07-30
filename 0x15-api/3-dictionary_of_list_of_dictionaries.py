2f640161894397a67b3cef78cb0f48e642bc8142
Dominic <usiabulu.dominic@outlook.com>
2024-07-30 04:20:59 +0100

    update api

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''
 A Python script that, using this REST API, for a given employee ID,
-export all information on tasks for all 
+export all information on tasks for all
 employees in the JSON format.
 '''
 import requests
