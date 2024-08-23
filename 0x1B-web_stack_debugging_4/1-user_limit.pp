cb99fdbcb5ef253a8a8b21f8bf2f37fb3140af06
Dominic <usiabulu.dominic@outlook.com>
2024-08-23 02:39:02 +0100

    debug 4

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,3 +1,4 @@
 exec {'change-os-config-holberton-user':
-  command => 
+  command => "sed -i 's/holberton hard nofile 5/holberton hard nofile 88888/' /etc/security/limits.conf; sed -i 's/holberton soft nofile 4/holberton soft nofile 88888/' /etc/security/limits.conf;",
+  path    => '/usr/bin:/usr/sbin:/bin'
 }
