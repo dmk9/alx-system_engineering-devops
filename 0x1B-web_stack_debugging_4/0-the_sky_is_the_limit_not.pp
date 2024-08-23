51dbf3621907bdef05c5276c29f6bfcc104889a8
Dominic <usiabulu.dominic@outlook.com>
2024-08-23 02:45:03 +0100

    debug 4

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 exec { 'nginx-ulimit-fix' :
-  command => "sed -i 's/^ULIMIT=.*/ULIMIT=\"-n 8192\"/' /etc/default/nginx; service nginx restart",
+  command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' \
+/etc/default/nginx; service nginx restart\"",
   path    => '/usr/bin:/usr/sbin:/bin'
-
 }
